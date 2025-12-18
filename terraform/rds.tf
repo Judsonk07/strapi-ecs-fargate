91f827a2d665e3acefce0b6cccbd1e7363ed5419
Judsonk07 <pappamuthupappamuthu@gmail.com>
2025-12-18 22:44:13 +0530

    first commit

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,21 +1,24 @@
-resource "aws_db_subnet_group" "strapi" {
-  name       = "strapi-db-subnet-group-v2"
-  subnet_ids = data.aws_subnets.default.ids
+resource "aws_security_group" "ecs_sg" {
+  name   = "strapi-ecs-sg"
+  vpc_id = data.aws_vpc.default.id
 
-  tags = {
-    Name = "strapi-db-subnet-group"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_security_group" "rds" {
-  name   = "strapi-rds-sg-v2"
+resource "aws_security_group" "rds_sg" {
+  name   = "strapi-rds-sg"
   vpc_id = data.aws_vpc.default.id
 
   ingress {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    security_groups = [aws_security_group.strapi.id] # ECS SG
+    security_groups = [aws_security_group.ecs_sg.id]
   }
 
   egress {
@@ -26,24 +29,29 @@ resource "aws_security_group" "rds" {
   }
 }
 
-resource "aws_db_instance" "strapi" {
+resource "aws_db_subnet_group" "strapi" {
+  name       = "strapi-db-subnet-group"
+  subnet_ids = data.aws_subnets.default.ids
+}
+
+resource "aws_db_instance" "strapi_db" {
   identifier              = "strapi-postgres-db"
   engine                  = "postgres"
   engine_version          = "14.10"
   instance_class          = "db.t3.micro"
   allocated_storage       = 20
 
-  db_name                 = "strapi"
-  username                = "strapi"
+  db_name                 = var.db_name
+  username                = var.db_user
   password                = var.db_password
 
   db_subnet_group_name    = aws_db_subnet_group.strapi.name
-  vpc_security_group_ids  = [aws_security_group.rds.id]
+  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
 
   publicly_accessible     = false
   skip_final_snapshot     = true
-
-  tags = {
-    Name = "strapi-rds"
-  }
 }
+
+# output "rds_endpoint" {
+#   value = aws_db_instance.strapi_db.endpoint
+# }
