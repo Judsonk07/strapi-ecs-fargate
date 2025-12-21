6c97eae6eea01bf8c3e590cbe98a0b3868ab6e35
Judsonk07 <pappamuthupappamuthu@gmail.com>
2025-12-21 21:30:38 +0530

    first commit

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,57 +1,23 @@
-resource "aws_security_group" "ecs_sg" {
-  name   = "strapi-ecs-sg"
-  vpc_id = data.aws_vpc.default.id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "rds_sg" {
-  name   = "strapi-rds-sg"
-  vpc_id = data.aws_vpc.default.id
-
-  ingress {
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    security_groups = [aws_security_group.ecs_sg.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_db_subnet_group" "strapi" {
   name       = "strapi-db-subnet-group"
   subnet_ids = data.aws_subnets.default.ids
 }
-
+ 
 resource "aws_db_instance" "strapi_db" {
   identifier              = "strapi-postgres-db"
   engine                  = "postgres"
   engine_version          = "14"
   instance_class          = "db.t3.micro"
   allocated_storage       = 20
-
+ 
   db_name                 = var.db_name
   username                = var.db_user
   password                = var.db_password
-
+ 
   db_subnet_group_name    = aws_db_subnet_group.strapi.name
-  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
-
+  # FIX: Now pointing to the correct SG defined in security_groups.tf
+  vpc_security_group_ids  = [aws_security_group.rds.id]
+ 
   publicly_accessible     = false
   skip_final_snapshot     = true
 }
-
-# output "rds_endpoint" {
-#   value = aws_db_instance.strapi_db.endpoint
-# }
--- terraform/security.tf
+++ terraform/security.tf
@@ -1,44 +1,19 @@
-# resource "aws_security_group" "strapi" {
-#   name   = "strapi-sg"
-#   vpc_id = data.aws_vpc.default.id
-
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   ingress {
-#     from_port       = 1337
-#     to_port         = 1337
-#     protocol        = "tcp"
-#     security_groups = [aws_security_group.alb.id]
-#   }
-
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-
 #####################################
 # ALB Security Group
+# Allows traffic from the internet to the Load Balancer
 #####################################
-
 resource "aws_security_group" "alb" {
-  name   = "strapi-alb-sg"
-  vpc_id = data.aws_vpc.default.id
-
+  name        = "strapi-alb-sg"
+  description = "Allow HTTP traffic from internet to ALB"
+  vpc_id      = data.aws_vpc.default.id
+ 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+ 
   egress {
     from_port   = 0
     to_port     = 0
@@ -46,22 +21,23 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
+ 
 #####################################
 # ECS (Strapi) Security Group
+# Allows traffic from the ALB to the ECS Task
 #####################################
-
 resource "aws_security_group" "strapi" {
-  name   = "strapi-ecs-sg-1"
-  vpc_id = data.aws_vpc.default.id
-
+  name        = "strapi-ecs-sg-1"
+  description = "Allow traffic from ALB to ECS"
+  vpc_id      = data.aws_vpc.default.id
+ 
   ingress {
     from_port       = 1337
     to_port         = 1337
     protocol        = "tcp"
     security_groups = [aws_security_group.alb.id]
   }
-
+ 
   egress {
     from_port   = 0
     to_port     = 0
@@ -69,22 +45,24 @@ resource "aws_security_group" "strapi" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
+ 
 #####################################
 # RDS Security Group
+# Allows traffic ONLY from the ECS Security Group
 #####################################
-
 resource "aws_security_group" "rds" {
-  name   = "strapi-rds-sg-1"
-  vpc_id = data.aws_vpc.default.id
-
+  name        = "strapi-rds-sg-1"
+  description = "Allow traffic from ECS to RDS"
+  vpc_id      = data.aws_vpc.default.id
+ 
   ingress {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
+    # This is the key fix: explicitly allowing the ECS SG defined above
     security_groups = [aws_security_group.strapi.id]
   }
-
+ 
   egress {
     from_port   = 0
     to_port     = 0
@@ -92,5 +70,4 @@ resource "aws_security_group" "rds" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
 
