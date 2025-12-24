8898fc8dea8ebd3123b8354e6f23f1ae743bf1ea
Judsonk07 <pappamuthupappamuthu@gmail.com>
2025-12-24 15:44:53 +0530

    Task 10 Completed

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -29,7 +29,7 @@ resource "aws_ecs_task_definition" "this" {
   memory                   = "1024"
 
   execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
-  # task_role_arn      = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn      = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
