26f9c439ba43645ed80f1d7db6ba47dab87dbdd1
Judsonk07 <pappamuthupappamuthu@gmail.com>
2025-12-29 10:20:12 +0530

    Task 10 Completed

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,49 +1,72 @@
 output "alb_url" {
-  value       = aws_lb.alb.dns_name
+
+  value = aws_lb.alb.dns_name
+
   description = "ALB DNS name for accessing Strapi"
+
 }
 
 output "alb_arn" {
-  value       = aws_lb.alb.arn
+
+  value = aws_lb.alb.arn
+
   description = "ALB ARN"
+
 }
 
 output "rds_endpoint" {
-  value       = aws_db_instance.strapi_db.endpoint
+
+  value = aws_db_instance.strapi_db.endpoint
+
   description = "RDS PostgreSQL endpoint"
+
 }
 
 output "rds_address" {
-  value       = aws_db_instance.strapi_db.address
+
+  value = aws_db_instance.strapi_db.address
+
   description = "RDS PostgreSQL address"
+
 }
 
 output "ecs_cluster_name" {
-  value       = aws_ecs_cluster.this.name
+
+  value = aws_ecs_cluster.this.name
+
   description = "ECS Cluster name"
+
 }
 
 output "ecs_service_name" {
-  value       = aws_ecs_service.this.name
+
+  value = aws_ecs_service.this.name
+
   description = "ECS Service name"
+
 }
 
 output "codedeploy_app_name" {
-  value       = aws_codedeploy_app.this.name
+
+  value = aws_codedeploy_app.this.name
+
   description = "CodeDeploy application name"
+
 }
 
 output "codedeploy_deployment_group" {
-  value       = aws_codedeploy_deployment_group.dg.deployment_group_name
+
+  value = aws_codedeploy_deployment_group.dg.deployment_group_name
+
   description = "CodeDeploy Deployment Group name"
+
 }
 
 output "alb_listener_http_arn" {
-  value       = aws_lb_listener.http.arn
+
+  value = aws_lb_listener.http.arn
+
   description = "ARN for ALB HTTP listener"
+
 }
 
-output "alb_listener_https_arn" {
-  value       = length(aws_lb_listener.https) > 0 ? aws_lb_listener.https[0].arn : ""
-  description = "ARN for ALB HTTPS listener (empty if no certificate provided)"
-}
