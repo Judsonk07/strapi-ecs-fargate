34f57de84013c6607c074323fc0d6f62b038a6e0
Judsonk07 <pappamuthupappamuthu@gmail.com>
2025-12-29 17:33:11 +0530

    Task 13 Completed

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
 
-  # CRITICAL: Create this bucket manually in AWS S3 before running
-  backend "s3" {
-    bucket = "my-strapi-state-bucket" # CHANGE THIS to your bucket name
-    key    = "strapi/terraform.tfstate"
-    region = "us-east-1"
-  }
+  # # CRITICAL: Create this bucket manually in AWS S3 before running
+  # backend "s3" {
+  #   bucket = "my-strapi-state-bucket" # CHANGE THIS to your bucket name
+  #   key    = "strapi/terraform.tfstate"
+  #   region = "us-east-1"
+  # }
 }
 
 provider "aws" {
