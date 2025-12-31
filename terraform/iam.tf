0a2d599d903c41cddc8917450d43923f393995f7
Judsonk07 <pappamuthupappamuthu@gmail.com>
2025-12-31 11:23:05 +0530

    Task 10 Completed

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,6 +1,6 @@
-# 1. Create IAM Role
+# 1. Create the IAM Role (Added suffix)
 resource "aws_iam_role" "ec2_ecr_role" {
-  name = "strapi_ec2_ecr_role-judson"
+  name = "strapi_role_${random_id.suffix.hex}"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -14,14 +14,14 @@ resource "aws_iam_role" "ec2_ecr_role" {
   })
 }
 
-# 2. Attach Policy to allow pulling from ECR
+# 2. Attach the policy
 resource "aws_iam_role_policy_attachment" "ecr_read_only" {
   role       = aws_iam_role.ec2_ecr_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-# 3. Create Instance Profile
+# 3. Create the Instance Profile (Added suffix)
 resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "strapi_ec2_profile"
+  name = "strapi_profile_${random_id.suffix.hex}"
   role = aws_iam_role.ec2_ecr_role.name
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,8 +3,12 @@ output "public_ip" {
   value       = aws_instance.strapi_server.public_ip
 }
 
+output "ssh_command" {
+  description = "Command to SSH into server"
+  value       = "ssh -i ${var.key_name}.pem ubuntu@${aws_instance.strapi_server.public_ip}"
+}
+
 output "strapi_url" {
   description = "URL to access Strapi"
   value       = "http://${aws_instance.strapi_server.public_ip}:1337"
 }
-
